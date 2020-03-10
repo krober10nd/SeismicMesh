da05aac8df2058993fc1e7cdd782615b4ded3487
Keith Roberts <krober@usp.br>
2020-03-10 06:08:14 -0300

    detecting nans in mesh size function

--- python/driver.py
+++ python/driver.py
@@ -4,21 +4,20 @@ import dmsh
 fname = 'vel_z6.25m_x12.5m_exact.segy'
 
 fh,nz,nx = ef.edgefx(bbox=(-12.0,0,0,67),segy=fname,
+            wl=5,freq=5,
             hmin=2e3,hmax=4e3)
-#ef.PlotMeshSizes(nz,nx,-12.0,67.0,fh )
+ef.PlotMeshSizes(nz,nx,-12.0,67.0,fh )
 
-geo = dmsh.Rectangle(-12.0,0.0,0,67.0)
+geo = dmsh.Rectangle(0.0,67.0,-12.0,0.0)
 
 def edge_size(x):
     import matplotlib.pyplot as plt
-    print(x[0,:].min())
-    print(x[0,:].max())
-    print(x[1,:].min())
-    print(x[1,:].max())
-    plt.plot(x[0,:],x[1,:])
+    import numpy as np
+    sizes = fh((x[1,:],x[0,:]))
+    sizes = np.nan_to_num(sizes,nan=1e3)
+    print(np.count_nonzero(np.isnan(sizes)))
+    plt.scatter(x[0,:],x[1,:],c = sizes)
     plt.show()
-    sizes = fh((x[0,:],x[1,:]))
-    print(sizes)
     return sizes
 
 X, cells = dmsh.generate(geo, edge_size, show=True, verbose=True)
