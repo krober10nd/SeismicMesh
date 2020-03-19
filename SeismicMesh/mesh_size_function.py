2bc37298241ec4b0ada62b3971c46404bd7001b5
Keith Roberts <krober10@nd.edu>
2020-03-19 14:34:53 -0300

    Format correction

--- SeismicMesh/mesh_size_function.py
+++ SeismicMesh/mesh_size_function.py
@@ -364,7 +364,7 @@ class MeshSizeFunction:
             )
         if _dim == 3:
             # x,y,z -> z,x,y
-            hh_m = hh_m.transpose((2,0,1))
+            hh_m = hh_m.transpose((2, 0, 1))
             interpolant = RegularGridInterpolator(
                 (z_vec, x_vec, y_vec), hh_m, bounds_error=False, fill_value=None
             )
