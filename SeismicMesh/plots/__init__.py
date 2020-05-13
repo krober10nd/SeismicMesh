6c7797ddae5fce4321fc0dd53f0a4969ae6587fc
Keith Roberts <krober10@nd.edu>
2020-05-13 18:03:14 -0300

    adding histogram of dihedral angles

--- SeismicMesh/plots/__init__.py
+++ SeismicMesh/plots/__init__.py
@@ -1,4 +1,5 @@
 from .simpplot import plot_tets, plot_facets
 from .points import plot_points3
+from .qual import dh_angles_hist
 
-__all__ = ["plot_tets", "plot_facets", "plot_points3"]
+__all__ = ["plot_tets", "plot_facets", "plot_points3", "dh_angles_hist"]
---
+++ SeismicMesh/plots/qual.py
@@ -0,0 +1,31 @@
+import numpy as np
+from math import log10, floor
+
+import matplotlib.pyplot as plt
+
+
+def dh_angles_hist(dh_angles):
+    """
+    Histograms of dihedral angles in tetrahedrons
+    """
+
+    def round_to_1(x):
+        return round(x, -int(floor(log10(abs(x)))))
+
+    n, bins, patches = plt.hist(
+        x=dh_angles, bins="auto", color="#0504aa", alpha=0.7, rwidth=0.85
+    )
+    maxN = np.amax(n)
+    minDh = np.amin(dh_angles)
+    maxDh = np.amax(dh_angles)
+    plt.plot(np.linspace(minDh, minDh, 10), np.linspace(0, maxN / 2, 10), "r-")
+    plt.plot(np.linspace(maxDh, maxDh, 10), np.linspace(0, maxN / 2, 10), "r-")
+    plt.text(minDh, maxN, str(round_to_1(minDh)))
+    plt.text(maxDh, maxN, str(round_to_1(maxDh)))
+    frame1 = plt.gca()
+    frame1.axes.yaxis.set_ticklabels([])
+    frame1.axes.yaxis.set_ticks([])
+    # Hide the right and top spines
+    frame1.spines["right"].set_visible(False)
+    frame1.spines["top"].set_visible(False)
+    plt.show()
