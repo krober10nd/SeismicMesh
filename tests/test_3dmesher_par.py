712c5b36cbe9ba113e171fbee43694357b859e6a
Keith Roberts <krober10@nd.edu>
2020-07-02 15:52:11 -0300

    updating 3d parallel test

--- tests/test_3dmesher_par.py
+++ tests/test_3dmesher_par.py
@@ -60,11 +60,38 @@ def test_3dpar_mesher():
 
         vol = SeismicMesh.geometry.simpvol(points / 1000, cells)
         assert np.abs(2 - np.sum(vol)) < 0.10  # km2
-        # 5865 vertices and 33063 cells
-        # 9220 49263
         print(len(points), len(cells))
-        assert np.abs(9220 - len(points)) < 100
-        assert np.abs(49263 - len(cells)) < 100
+        assert np.abs(9220 - len(points)) < 250
+        assert np.abs(49263 - len(cells)) < 250
+
+    mshgen = SeismicMesh.MeshGenerator(
+        ef, method="cgal"
+    )  # parallel currently only works in qhull
+
+    points, cells = mshgen.build(max_iter=50, nscreen=1, seed=0, COMM=comm, axis=0,)
+    # remove slivers
+    points, cells = mshgen.build(
+        points=points,
+        max_iter=15,
+        nscreen=1,
+        seed=0,
+        COMM=comm,
+        axis=0,
+        mesh_improvement=True,
+    )
+
+    if rank == 0:
+        # import meshio
+
+        # meshio.write_points_cells(
+        #    "foo3D_V3.vtk", points, [("tetra", cells)],
+        # )
+
+        vol = SeismicMesh.geometry.simpvol(points / 1000, cells)
+        assert np.abs(2 - np.sum(vol)) < 0.10  # km2
+        print(len(points), len(cells))
+        assert np.abs(9220 - len(points)) < 250
+        assert np.abs(49263 - len(cells)) < 250
 
 
 if __name__ == "__main__":
