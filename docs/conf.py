9e7e7877d193fe2c5baa29efd58a03ef07f5aa9a
Keith Roberts <krober10@nd.edu>
2020-07-06 15:14:08 -0300

    master doc is index

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'SeismicMesh'
-copyright = '2020, Keith Jared Roberts'
-author = 'Keith Jared Roberts'
+project = "SeismicMesh"
+copyright = "2020, Keith Jared Roberts"
+author = "Keith Jared Roberts"
 
 # The full version, including alpha/beta/rc tags
-release = 'V1.0'
+release = "V1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,18 +30,18 @@ release = 'V1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
+
+
+master_doc = "index"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-autodoc_mock_imports = ['mpi4py','h5py']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,9 +49,9 @@ autodoc_mock_imports = ['mpi4py','h5py']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
--- docs/index.rst
+++ docs/index.rst
@@ -3,18 +3,44 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to SeismicMesh's documentation!
-=======================================
+SeismicMesh
+========
 
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+$project will solve your problem of where to start with documentation,
+by providing a basic explanation of how to do it easily.
 
+Look how easy it is to use:
 
+    import project
+    # Get your stuff done
+    project.do_stuff()
 
-Indices and tables
-==================
+Features
+--------
 
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+- Be awesome
+- Make things faster
+
+Installation
+------------
+
+Install $project by running:
+
+    install project
+
+Contribute
+----------
+
+- Issue Tracker: github.com/$project/$project/issues
+- Source Code: github.com/$project/$project
+
+Support
+-------
+
+If you are having issues, please let us know.
+We have a mailing list located at: project@google-groups.com
+
+License
+-------
+
+The project is licensed under the BSD license.
