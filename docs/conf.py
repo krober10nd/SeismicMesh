55b00833287814681cb4f52a5d7f60d165231040
Keith Roberts <krober10@nd.edu>
2020-07-06 13:39:30 -0300

    mocking mpi4py for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autodoc_mock_imports = ["mpi4py"]
+
 
 # -- Options for HTML output -------------------------------------------------
 
