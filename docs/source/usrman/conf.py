ed35c73376043c1216ac2f891a688439fab3c005
Keith Roberts <krober10@nd.edu>
2020-07-08 20:51:28 -0300

    update conf

--- docs/source/usrman/conf.py
+++ docs/source/usrman/conf.py
@@ -33,7 +33,7 @@ sys.path.insert(0, os.path.abspath("../../../"))
 # ones.
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
 
-autodoc_mock_imports = ["h5py", "mpi4py", "fast_geometry", "c_cgal", "cpp"]
+autodoc_mock_imports = ["h5py", "mpi4py", "geometry"]
 
 master_doc = "index"
 
