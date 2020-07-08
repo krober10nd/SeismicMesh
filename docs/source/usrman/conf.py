bfd0797a2546295f2941fe150265d41780433b98
Keith Roberts <krober10@nd.edu>
2020-07-08 19:16:21 -0300

    more mocks

--- docs/source/usrman/conf.py
+++ docs/source/usrman/conf.py
@@ -33,7 +33,7 @@ sys.path.insert(0, os.path.abspath("../../../"))
 # ones.
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
 
-autodoc_mock_imports = ["h5py", "mpi4py"]
+autodoc_mock_imports = ["h5py", "mpi4py", "fast_geometry", "c_cgal"]
 
 master_doc = "index"
 
