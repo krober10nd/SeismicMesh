4195c6ec6a9083c4f4b49129f6355751a1d6b274
Keith Roberts <krober10@nd.edu>
2020-07-22 19:01:31 -0300

    inc. point add, remove, and move working

--- SeismicMesh/generation/cpp/delaunay_class.cpp
+++ SeismicMesh/generation/cpp/delaunay_class.cpp
@@ -26,6 +26,7 @@ using DT = CGAL::Delaunay_triangulation_2<K, Tds>;
 
 using Point = K::Point_2;
 using Vertex_handle = DT::Vertex_handle;
+using Vi = DT::Vertex_iterator;
 
 
 template <typename T>
@@ -104,11 +105,14 @@ PYBIND11_MODULE(delaunay_class, m)
         "point",
         [](const Vertex_handle& vertex_handle)
         {
-            return vertex_handle->info();
+            return vertex_handle->point();
         })
-            ;
-    py::class_<DT>(m, "DelaunayTriangulation")
+        ;
+
+        py::class_<DT>(m, "DelaunayTriangulation")
+
             .def(py::init())
+
             .def("insert", [](DT & dt, const std::vector<double> & p) {
                   std::vector< std::pair<Point,unsigned> > points;
                   int num_points = p.size()/2;
@@ -122,40 +126,112 @@ PYBIND11_MODULE(delaunay_class, m)
                   }
                   return dt.insert(points.begin(),points.end());
                 })
-            .def("remove", &DT::remove)
+
+            .def("remove", [](DT & dt, const std::vector<unsigned int> & to_remove) {
+                    int num_to_remove= to_remove.size();
+                    std::vector<Vertex_handle> handles;
+                    for (Vi vi = dt.vertices_begin(); vi != dt.vertices_end(); vi++){
+                        handles.push_back(vi);
+                    }
+                    for(std::size_t i=0; i < num_to_remove; ++i){
+                        dt.remove(handles[to_remove[i]]);
+                    }
+                    return dt;
+                })
+
+            .def("move", [](DT & dt, const std::vector<unsigned int> & to_move, const std::vector<double> & new_positions){
+                    std::vector<Vertex_handle> handles;
+                    std::vector<Point> new_pos;
+                    int num_to_move= to_move.size();
+                    // store all vertex handles
+                    for (Vi vi = dt.vertices_begin(); vi != dt.vertices_end(); vi++){
+                        handles.push_back(vi);
+                    }
+                    // store new positions as a vector of Point
+                    for(std::size_t i = 0; i < num_to_move; ++i)
+                    {
+                       new_pos.push_back(Point(new_positions[2*i], new_positions[2*i+1]));
+                    }
+                    //
+                    for(std::size_t i = 0; i < num_to_move; ++i)
+                    {
+                       dt.move( handles[to_move[i]], new_pos[i] );
+                    }
+                    return dt;
+                    })
+
             .def("number_of_vertices", &DT::number_of_vertices)
+
             .def("number_of_faces", &DT::number_of_faces)
+
             .def("finite_vertices", [](DT & dt) -> py::iterator
              {
                  return py::make_iterator(dt.finite_vertices_begin(), dt.finite_vertices_end());
              })
-             .def("finite_faces", [](DT & dt)
+
+            .def("get_finite_faces", [](DT & dt)
+            {
+              // ouput the face table
+              // YOU MUST CALL get_finite_vertices before if any incremental operations
+              // were performed
+              std::vector<int> faces;
+              faces.resize(dt.number_of_faces()*3);
+
+              int i=0;
+              for(DT::Finite_faces_iterator fit = dt.finite_faces_begin();
+                fit != dt.finite_faces_end(); ++fit) {
+
+                DT::Face_handle face = fit;
+                faces[i*3]=face->vertex(0)->info();
+                faces[i*3+1]=face->vertex(1)->info();
+                faces[i*3+2]=face->vertex(2)->info();
+                i+=1;
+              }
+              ssize_t              soint      = sizeof(int);
+              ssize_t              num_faces = faces.size()/3;
+              ssize_t              ndim      = 2;
+              std::vector<ssize_t> shape     = {num_faces, 3};
+              std::vector<ssize_t> strides   = {soint*3, soint};
+
+              // return 2-D NumPy array
+              return py::array(py::buffer_info(
+                faces.data(),                           /* data as contiguous array  */
+                sizeof(int),                          /* size of one scalar        */
+                py::format_descriptor<int>::format(), /* data type                 */
+                2,                                    /* number of dimensions      */
+                shape,                                   /* shape of the matrix       */
+                strides                                  /* strides for each axis     */
+              ));
+            })
+
+            .def("get_finite_vertices", [](DT & dt)
              {
-               // ouput the face table
-               std::vector<int> faces;
-               faces.resize(dt.number_of_faces()*3);
+               // ouput the vertices
+               std::vector<double> vertices;
+               vertices.resize(dt.number_of_vertices()*2);
 
                int i=0;
-               for(DT::Finite_faces_iterator fit = dt.finite_faces_begin();
-                 fit != dt.finite_faces_end(); ++fit) {
+               for(DT::Finite_vertices_iterator fit = dt.finite_vertices_begin();
+                 fit != dt.finite_vertices_end(); ++fit) {
 
-                 DT::Face_handle face = fit;
-                 faces[i*3]=face->vertex(0)->info();
-                 faces[i*3+1]=face->vertex(1)->info();
-                 faces[i*3+2]=face->vertex(2)->info();
+                 DT::Vertex_handle vertex = fit;
+                 // critical! update the point index table so faces comes out correctly
+                 vertex->info() = i;
+                 vertices[i*2]=vertex->point().x();
+                 vertices[i*2+1]=vertex->point().y();
                  i+=1;
                }
-               ssize_t              soint      = sizeof(int);
-               ssize_t              num_faces = faces.size()/3;
+               ssize_t              sdble   = sizeof(double);
+               ssize_t              num_vertices = vertices.size()/2;
                ssize_t              ndim      = 2;
-               std::vector<ssize_t> shape     = {num_faces, 3};
-               std::vector<ssize_t> strides   = {soint*3, soint};
+               std::vector<ssize_t> shape     = {num_vertices, 2};
+               std::vector<ssize_t> strides   = {sdble*2, sdble};
 
                // return 2-D NumPy array
                return py::array(py::buffer_info(
-                 faces.data(),                           /* data as contiguous array  */
-                 sizeof(int),                          /* size of one scalar        */
-                 py::format_descriptor<int>::format(), /* data type                 */
+                 vertices.data(),                           /* data as contiguous array  */
+                 sizeof(double),                          /* size of one scalar        */
+                 py::format_descriptor<double>::format(), /* data type                 */
                  2,                                    /* number of dimensions      */
                  shape,                                   /* shape of the matrix       */
                  strides                                  /* strides for each axis     */
