fb270d3e354776f6141f24c1267910739aaef63d
Keith Roberts <krober10@nd.edu>
2020-08-28 15:44:42 -0300

    marking as a serial test

--- tests/test_README.py
+++ tests/test_README.py
@@ -9,6 +9,7 @@ import pytest
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
+@pytest.mark.serial
 @pytest.mark.parametrize(
     "string,lineno",
     exdown.extract(this_dir.parent / "README.md", syntax_filter="python"),
