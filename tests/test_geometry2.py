ff72d36ac71fed0475cfa0a8d0104d064e0cfc5e
keith roberts <krober@usp.br>
2020-08-31 19:05:56 -0300

    Adding one more CI test (#41)
    * More testing of geometry.

--- tests/test_geometry2.py
+++ tests/test_geometry2.py
@@ -22,6 +22,9 @@ def test_geometry2():
     wedges = geo.get_winded_boundary_edges(cells)
     assert np.allclose(wedges, [[0, 1], [1, 3], [2, 3], [0, 2]])
 
+    intersections = geo.doAnyOverlap(points, cells, dim=2)
+    assert len(intersections) == 0
+
     # test Laplacian2
     pdata = os.path.join(os.path.dirname(__file__), "points.txt")
     cdata = os.path.join(os.path.dirname(__file__), "cells.txt")
