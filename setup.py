58862e0123738586bf6033032555009640641a6a
Keith Roberts <krober10@nd.edu>
2020-11-25 09:31:08 -0300

    removing block

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ if sys.version_info < (3, 0):
     print("Python 3.0 or higher required, please upgrade.")
     sys.exit(1)
 
-if sys.version_info > (3, 8):
+if sys.version_info >= (3, 9, 0):
     print(
-        "Python 3.9 or higher is not yet supported, please use an older Python version."
+        "Python 3.9.0 or higher is not yet supported, please use an older Python version."
     )
     sys.exit(1)
 
